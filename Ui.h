9e83b41d291850583d860075319bcee1671bc210
Ren Akamine <k024g0059@g.neec.ac.jp>
2024-11-19 15:33:36 +0900

    HPゲージの描画関数(仮)の追加

---
+++ Ui.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <Novice.h>
+#include "Data.h"
+
+class Ui {
+public:
+
+	/// <summary>
+	/// HPゲージの描画関数
+	/// </summary>
+	/// <param name="posX">描画位置X</param>
+	/// <param name="posY">描画位置Y</param>
+	/// <param name="hp">対象のHP</param>
+	/// <param name="type">0 = 敵, 1 = プレイヤー</param>
+	void DrawHpBar(const int& posX, const int& posY, const int& hp, const int& type);
+};
