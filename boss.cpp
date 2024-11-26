77cf7920adbd18008b30eee9f6aecb499c349bc2
HARU SHIMIZUGUCHI <k024g0026@g.neec.ac.jp>
2024-11-26 13:01:04 +0900

    エリア全体描画

--- boss.cpp
+++ boss.cpp
@@ -38,7 +38,7 @@ void Boss::BossMove(Boss_& boss, BossRengeAttak_& renge, ShortDistansAttak_& sho
 			if (boss.attakNo == 0) {
 				if (boss.attakStandTime <= 0) {
 					//boss.attakNo = rand() % 5 + 1;
-					boss.attakNo = 40;
+					boss.attakNo = 12;
 					/*if (boss.hp > 100) {
 						boss.attakNo = 5;
 					}*/
